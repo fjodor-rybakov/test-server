25a0a145affcd0474705173ee9c4e6a664529b1e
fjodor-rybakov <fedryb@mail.ru>
2019-01-20 17:31:46 +0300

    add is email

--- Services/ProfileServices.js
+++ Services/ProfileServices.js
@@ -1,4 +1,5 @@
 import * as errors from "restify-errors";
+import Utils from "../Utils/Utils";
 
 export class ProfileServices {
     async getProfile(database, id_user) {
@@ -19,7 +20,7 @@ export class ProfileServices {
     async updateProfile(database, data, path) {
         const {first_name, last_name, email, id_user} = data;
 
-        if (!email.match(`(?:[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])`)) {
+        if (!Utils.isEmail(email)) {
             throw new errors.InvalidArgumentError("Incorrect email address");
         }
 
